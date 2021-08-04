855f8435ea7828745b18d43a6d9cd0f916dca8c5
Roberto Romolini <romolini.roberto@gmail.com>
2021-08-04 10:55:39 +0200

    Add settings const class

---
+++ WindowWidgets/Settings/settingsconst.cpp
@@ -0,0 +1,17 @@
+#include "settingsconst.h"
+
+SettingsConst::SettingsConst() { }
+
+const QString SettingsConst::directoryBackup = "directoryBackup";
+const QString SettingsConst::directoryMemory = "directoryMemory";
+const QString SettingsConst::percAumento = "percAumento";
+const QString SettingsConst::qualitaSalvataggio = "qualitaSalvataggio";
+const QString SettingsConst::trasformaQualita = "trasformaQualita";
+const QString SettingsConst::latoMin = "latoMin";
+const QString SettingsConst::latoMax = "latoMax";
+const QString SettingsConst::ridimensionaMin = "ridimensionaMin";
+const QString SettingsConst::ridimensionaMax = "ridimensionaMax";
+const QString SettingsConst::ratioWidth = "ratioWidth";
+const QString SettingsConst::ratioHeight = "ratioHeight";
+const QString SettingsConst::apiKeyRemoveBG = "apiKeyRemoveBG";
+const QString SettingsConst::urlRemoveBG = "urlRemoveBG";
