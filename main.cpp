2eb8bbf70018be757da139ee80414d923af210b2
Roberto Romolini <r.romolini@sintraconsulting.eu>
2021-08-19 11:05:44 +0200

    Add global QSettings

--- main.cpp
+++ main.cpp
@@ -4,6 +4,9 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QSettings::setDefaultFormat(QSettings::IniFormat);
+    QApplication::setOrganizationName("FotoMaster");
+    QApplication::setApplicationName("settings");
     MainWindow w;
     w.show();
     return a.exec();
